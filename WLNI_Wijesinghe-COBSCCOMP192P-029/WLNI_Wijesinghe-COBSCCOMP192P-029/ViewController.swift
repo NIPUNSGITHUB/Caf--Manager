4002cfb0d549ffc5e967e9a629cb1c7a3cf2c9a5
Nipunishara <nipunishara.info@gmail.com>
2021-04-24 12:14:48 +0530

    first commit

---
+++ WLNI_Wijesinghe-COBSCCOMP192P-029/WLNI_Wijesinghe-COBSCCOMP192P-029/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WLNI_Wijesinghe-COBSCCOMP192P-029
+//
+//  Created by Macbook on 4/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
